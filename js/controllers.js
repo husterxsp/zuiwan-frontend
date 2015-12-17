b4f694cab487ad75183d08986909cb3caaf18bed
李冰涛 <libingtao@libingtaodeMacBook-Pro.local>
2015-12-17 11:45:34 +0800

    article from server; collect article

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,7 @@
 //推荐模块
 var recommendModule = angular.module("RecommendModule", []);
-recommendModule.controller('RecommendCtrl', function($scope) {
+recommendModule.controller('RecommendCtrl', function($scope, $http, ArticleService) {
 
-    console.log(document.getElementById('slide'));
     $scope.mySwipe = new Swipe(document.getElementById('slide'), {
         startSlide: 2,
         speed: 400,
@@ -13,7 +12,14 @@ recommendModule.controller('RecommendCtrl', function($scope) {
         callback: function(index, elem) {},
         transitionEnd: function(index, elem) {}
     });
-
+    var promiseOrArticles = ArticleService.query();
+    if (promiseOrArticles.then){
+        promiseOrArticles.then(function(data) {
+            $scope.articles = data;
+        });
+    } else {
+        $scope.articles = promiseOrArticles;
+    }
 });
 
 //专题模块
@@ -91,8 +97,43 @@ accountModule.controller('AccountCtrl', function($scope, $http, $state, Storage)
                 })
         };
     }]);
-//文章
-var articleModule = angular.module("ArticleModule", []);
-articleModule.controller('ArticleCtrl', function($scope) {
 
+//文章
+var articleModule = angular.module("ArticleModule", ["ngSanitize"]);
+articleModule.controller('ArticleCtrl', function($scope, $http, $state, Storage, ArticleService) {
+    var id = $state.params.articleId;
+    var promiseOrArticles = ArticleService.query();
+    if (promiseOrArticles.then){
+        promiseOrArticles.then(function(data) {
+            $scope.articles = data;
+            $scope.article = $scope.articles[id];
+        });
+    } else {
+        $scope.articles = promiseOrArticles;
+        $scope.article = $scope.articles[id];
+    }
+    $scope.collect = function(){
+        /**
+         * @todo user不存在时，collect按钮不应该显示
+         */
+        var user = Storage.get();
+        if (!user){
+            return;
+        }
+        $http.post("/zuiwan-backend/index.php/user/collect_article", {
+            username: Storage.get().username,
+            article_id: $scope.article.id
+        }, {
+            withCredentials: true
+        })
+        .then(function(res){
+            if (res.data.message == "success") {
+                //提示收藏文章成功
+            } else {
+                //收藏文章失败
+            }
+        }, function(res) {
+            alert("注册失败！");
+        })
+    }
 });
--- js/services.js
+++ js/services.js
@@ -20,3 +20,24 @@ angular.module('services', [])
         }
     };
 })
+
+.factory('ArticleService', ['$http', '$q', function ($http, $q) {
+    var articles;
+    return {
+        query : function() {
+            if (articles){
+                return articles;
+            }
+            var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
+            $http({method: 'GET', url: 'http://localhost/zuiwan-backend/index.php/article/get_article'})
+            .success(function(data, status, headers, config) {
+                articles = data.articles;
+                deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
+            })
+            .error(function(data, status, headers, config) {
+                deferred.reject(data);   // 声明执行失败，即服务器返回错误
+            });
+            return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+        }
+    };
+}]);
